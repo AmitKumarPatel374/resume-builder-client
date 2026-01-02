c4d3f13442bf92546a7b055d96dc2095c310a35a
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2026-01-02 11:50:58 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import ResumeBuilder from "./pages/ResumeBuilder";
+import Preview from "./pages/Preview";
+import Login from "./pages/Login";
+
+const App = () => {
+  return (
+    <Routes>
+      {/* Public routes */}
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/view/:resumeId" element={<Preview />} />
+
+      {/* App layout routes */}
+      <Route path="/app" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="builder/:resumeId" element={<ResumeBuilder />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
